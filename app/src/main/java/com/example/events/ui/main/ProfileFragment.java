081b3b70b66d0be3363a8ab7de286019df31e565
1ntr0men <vadruf07@gmail.com>
2021-05-20 13:41:31 +0300

    Фикс коммит 1

--- app/src/main/java/com/example/events/ui/main/ProfileFragment.java
+++ app/src/main/java/com/example/events/ui/main/ProfileFragment.java
@@ -2,30 +2,13 @@ package com.example.events.ui.main;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.events.MainActivity;
 import com.example.events.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,10 +16,6 @@ import static android.content.ContentValues.TAG;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
-
-    MainActivity m;
-
-    private FirebaseAuth mAuth;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -46,50 +25,6 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
-
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        //Buttons
-        Button sign_in_button = view.findViewById(R.id.signInButton);
-        Button sign_up_button = view.findViewById(R.id.signUpButton);
-
-        EditText email_line = view.findViewById(R.id.emailInput);
-        EditText password_line = view.findViewById(R.id.passwordInput);
-
-        TextView TESTTEST = view.findViewById(R.id.main_title);
-
-        sign_in_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = email_line.getText().toString();
-                String password = password_line.getText().toString();
-                TESTTEST.setText("WORK");
-                SignIn(email, password);
-            }
-        });
-
-        sign_up_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = email_line.getText().toString();
-                String password = password_line.getText().toString();
-                Log.i(TAG, "mi zdes");
-                SignUp(email, password);
-            }
-        });
-
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-    }
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -101,7 +36,7 @@ public class ProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ManFragment.
+     * @return A new instance of fragment ProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
@@ -122,29 +57,10 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-
-    private void SignIn(String email, String password) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
-
-    private void SignUp(String email, String password) {
-
-        Log.i(TAG, "signUp:" + email);
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                        } else {
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(getContext(), "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                });
-    }
-
 }
---
+++ app/src/main/java/com/example/events/ui/main/SignInFragment.java
@@ -0,0 +1,177 @@
+package com.example.events.ui.main;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.events.MainActivity;
+import com.example.events.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SignInFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SignInFragment extends Fragment {
+
+    MainActivity m;
+
+    private FirebaseAuth mAuth;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.sign_in_fragment, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Buttons
+        Button sign_in_button = view.findViewById(R.id.signInButton);
+        Button sign_up_button = view.findViewById(R.id.signUpButton);
+
+        EditText email_line = view.findViewById(R.id.emailInput);
+        EditText password_line = view.findViewById(R.id.passwordInput);
+
+
+
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_line.getText().toString();
+                String password = password_line.getText().toString();
+                SignIn(email, password, view);
+            }
+        });
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_line.getText().toString();
+                String password = password_line.getText().toString();
+                SignUp(email, password, view);
+            }
+        });
+
+
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public SignInFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ManFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SignInFragment newInstance(String param1, String param2) {
+        SignInFragment fragment = new SignInFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+
+    private void SignIn(String email, String password, View view) {
+//        Log.i(TAG, "signIn:" + email);
+//
+//        mAuth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//
+////                        } else {
+////                            // If sign in fails, display a message to the user.
+////                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+////                            Toast.makeText(getContext(), "Authentication failed.",
+////                                    Toast.LENGTH_SHORT).show();
+////                            updateUI(null);
+////                            checkForMultiFactorFailure(task.getException());
+////                        }
+////
+////                        if (!task.isSuccessful()) {
+////                            mBinding.status.setText(R.string.auth_failed);
+////                        }
+////                        hideProgressBar();
+//                    }
+//                });
+    }
+
+    private void SignUp(String email, String password, View view) {
+        TextView message = view.findViewById(R.id.message_text_view);
+
+        Log.i(TAG, "signUp:" + email);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            message.setText("Sign up successful. Please verificate your account in mail");
+
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            message.setText("Sign up failed");
+
+                        }
+                    }
+                });
+    }
+}
